b5b94f23fd9b9898221631ebd89d7d11b1140efa
Emilly Poliane <emilly.carmo@dcx.ufpb.br>
2024-11-08 19:51:53 -0300

    first commit

---
+++ src/Aula06 - estruturas-de-dados.py
@@ -0,0 +1,3 @@
+lista = ['Maria','João'] #List --> lista de nomes
+tupla = ('Maria' 'João') #Tuple --> uma lista com uma quantidade fixa
+dicionario = {'nome:':'João', 'idade':23} #Dict --> HashMap, chave e valor
